fbb3fe355abd840cbf0ca065fec68482f9ebfd26
Angus <sugnalolz2@gmail.com>
2025-05-08 04:13:40 +0800

    250508

---
+++ src/main.js
@@ -0,0 +1,11 @@
+//載入 createApp Function
+import { createApp } from "vue";
+
+//載入根組件
+import App from "./App.vue";
+
+//建立 Vue App 物件
+const app=createApp(App);
+
+//掛載到 HTML 標籤底下
+app.mount("#app");
